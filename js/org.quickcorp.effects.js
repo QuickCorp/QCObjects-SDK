d231ca2aee3e5fe52ebc5f8578e6f1a8c58afe82
Jean Machuca <correojean@gmail.com>
2019-04-25 15:52:57 -0400

    move Effect  to QCObjects

--- js/org.quickcorp.effects.js
+++ js/org.quickcorp.effects.js
@@ -25,28 +25,6 @@
 "use strict";
 (function() {
   Package('org.quickcorp.tools.effects',[
-    Class('Effect',{
-      animate: function ({timing, draw, duration}) {
-
-        let start = performance.now();
-
-        requestAnimationFrame(function animate(time) {
-          // timeFraction goes from 0 to 1
-          let timeFraction = (time - start) / duration;
-          if (timeFraction > 1) timeFraction = 1;
-
-          // calculate the current animation state
-          let progress = timing(timeFraction);
-
-          draw(Math.round(progress*100)); // draw it
-
-          if (timeFraction < 1) {
-            requestAnimationFrame(animate);
-          }
-
-        });
-      }
-    }),
     Class('Move',Effect,{
       apply: function (element, xfrom, yfrom, xto, yto){
         var dx = xto-xfrom;
