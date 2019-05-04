deb9ab62f0eff189f5857c7ea67a266f215e2e02
Jean Machuca <correojean@gmail.com>
2019-05-03 23:02:09 -0400

    basic-layout

--- js/org.quickcorp.views.js
+++ js/org.quickcorp.views.js
@@ -1,5 +1,19 @@
 'use strict';
 Package('org.quickcorp.views',[
+  Class('MainView',View,{
+    dependencies:[
+      New(SourceCSS,{
+        external:false,
+        url:'css/basic-layout.css'
+      })
+    ],
+    component:null,
+    _new_:function (o){
+      this.__new__(o);
+      var controller=this;
+      //TODO: Implement
+    }
+  }),
   Class('GridView',View,{
     dependencies:[],
     component:null,
