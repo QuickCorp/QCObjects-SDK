577781e702ae4aa7478dfee7e3f528e580d1532b
Jean Machuca <correojean@gmail.com>
2019-05-03 23:41:30 -0400

    BasicLayout

--- js/org.quickcorp.views.js
+++ js/org.quickcorp.views.js
@@ -1,19 +1,5 @@
 'use strict';
 Package('org.quickcorp.views',[
-  Class('BasicLayoutView',Object,{
-    dependencies:[],
-    component:null,
-    _new_:function (o){
-      if (o.hasOwnProperty('component')){
-        this.component = o.component;
-      }
-      var controller=this;
-      this.dependencies.push(New(SourceCSS,{
-        external:(CONFIG.get('useLocalSDK'))?(false):(true),
-        url:(CONFIG.get('useLocalSDK'))?('css/basic-layout.css'):(CONFIG.get('remoteSDKPath')+'css/basic-layout.css')
-      }));
-    }
-  }),
   Class('GridView',View,{
     dependencies:[],
     component:null,
