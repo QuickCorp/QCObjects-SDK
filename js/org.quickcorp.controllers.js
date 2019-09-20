160998f6d2caee90bb0982a1cdbaf1bdbf53d713
Jean Machuca <correojean@gmail.com>
2019-09-19 22:22:56 -0300

    elementary classes to work with modals

--- js/org.quickcorp.controllers.js
+++ js/org.quickcorp.controllers.js
@@ -25,5 +25,19 @@ Package('org.quickcorp.controllers',[
       logger.debug('GridComponent built');
 
     }
-  })
+  }),
+  Class('ModalController',Controller,{
+    dependencies:[],
+    component:null,
+    _new_:function (o){
+      this.__new__(o);
+      var controller=this;
+      //TODO: Implement
+    },
+    done: function (){
+      var component = this.component;
+      component.body.innerHTML = component.body.innerHTML.replace('{{content}}',component.submodal.template);
+
+    }
+  })  
 ]);
