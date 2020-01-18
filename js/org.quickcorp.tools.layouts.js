86b907eb2378f92442f18e0dddd02c67736c4ee1
Jean Machuca <correojean@gmail.com>
2020-01-18 13:54:38 -0300

    colored border helper function for BasicLayout

--- js/org.quickcorp.tools.layouts.js
+++ js/org.quickcorp.tools.layouts.js
@@ -33,6 +33,37 @@
           external:(CONFIG.get('useLocalSDK'))?(false):(true),
           url:(CONFIG.get('useLocalSDK'))?('css/basic-layout.css'):(CONFIG.get('remoteSDKPath')+'css/basic-layout.css')
         }));
+      },
+      coloredBorder:function (){
+        /*
+        * A helper function to visualize the layout borders
+        * Usage: BasicLayout.coloredBorder()
+        */
+        setTimeout(function (){
+          Tag('nav').map(element=>element.style.border='20px solid #3333')
+          Tag('nav').map(element=>element.style.backgroundColor='#129999')
+          Tag('component>footer').map(element=>element.style.background='#876')
+          Tag('component>div').map(element=>element.style.border='3px dashed #fff')
+          Tag('component>section').map(element=>element.style.border='3px solid #000')
+          Tag('component>section').map(element=>element.style.backgroundColor='#fffaaa')
+
+          Tag('component>article').map(element=>element.style.border='3px dotted #000')
+          Tag('component>header').map(element=>element.style.background='#789')
+          Tag('component>footer').map(element=>element.style.background='#876')
+          Tag('component>article:nth-child(1)').map(element=>element.style.border='1px solid #444')
+          Tag('component>article:nth-child(1)').map(element=>element.style.backgroundColor='#555aaa')
+          Tag('component>article:nth-child(2)').map(element=>element.style.backgroundColor='#aaa333')
+          Tag('component>article:nth-child(3)').map(element=>element.style.backgroundColor='#54da82')
+          Tag('*').map(element=>element.style.color='#fff')
+
+
+          Tag('component>article').map(element=>Fade.apply(element,0,1))
+          Tag('component>footer').map(element=>Fade.apply(element,0,1))
+          Tag('component>header').map(element=>Fade.apply(element,0,1))
+          Tag('nav').map(element=>{element.style.display='block';element.width=element.offsetParent.scrollWidth;MoveXInFromLeft.apply(element);})
+          Tag('component>article').map(element=>{element.style.display='block';element.height=element.offsetParent.scrollHeight;MoveYInFromBottom.apply(element);})
+          Tag('component>article:nth-child(2)').map(element=>{element.style.display='block';element.width=element.offsetParent.scrollWidth;MoveXInFromRight.apply(element);})
+        },300);
       }
     })
   ]);
