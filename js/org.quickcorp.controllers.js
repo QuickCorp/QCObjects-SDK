1b7ea813931740e6c0c89d607c087ac682b244aa
Jean Machuca <correojean@gmail.com>
2020-01-31 18:26:46 -0300

    DataGridController for components

--- js/org.quickcorp.controllers.js
+++ js/org.quickcorp.controllers.js
@@ -26,6 +26,78 @@ Package('org.quickcorp.controllers',[
 
     }
   }),
+  Class('DataGridController',Controller,{
+    dependencies:[],
+    component:null,
+    _new_:function (o){
+      var controller=this;
+      //TODO: Implement
+      logger.debug('DataGridController INIT');
+    },
+    addSubcomponents:function (){
+      var controller = this;
+      controller.component.subcomponents = [];
+      controller.component.body.innerHTML = '';
+      console.log(controller.component.data);
+      try {
+        controller.component.data.map(
+          function (record){
+            var subcomponentClass = controller.component.body.getAttribute('subcomponentClass');
+            if (subcomponentClass != null){
+              try {
+                var subcomponent = New(ClassFactory(subcomponentClass),{
+                  data:record,
+                  templateURI:ComponentURI({
+                    'COMPONENTS_BASE_PATH':CONFIG.get('componentsBasePath'),
+                    'COMPONENT_NAME':ClassFactory(subcomponentClass).name,
+                    'TPLEXTENSION':CONFIG.get('tplextension'),
+                    'TPL_SOURCE':'default' //here is always default in order to get the right uri
+                  }),
+                  body:document.createElement('component')
+                });
+                controller.component.subcomponents.push(subcomponent);
+                controller.component.body.append(subcomponent.body);
+
+              } catch (e) {
+                logger.debug('ERROR LOADING SUBCOMPONENT IN DATAGRID');
+              }
+            } else {
+              logger.debug('NO SUBCOMPONENT CLASS IN COMPONENT');
+            }
+          }
+        );
+
+      } catch (e){
+        logger.debug('No data for component');
+      }
+    },
+    done:function (){
+      var componentInstance = this.component;
+      logger.debug('DataGridController DONE');
+      var serviceClass = controller.component.body.getAttribute('serviceClass');
+      if (serviceClass != ){
+        var service = serviceLoader(New(ClassFactory(serviceClass),{
+            data:{param1:1}
+        })).then(
+          (successfulResponse)=>{
+            // This will show the service response as a plain text
+            console.log('DONE SERVICE COMPONENT');
+            console.log(successfulResponse.service.JSONresponse.result);
+            componentInstance.data = successfulResponse.service.JSONresponse.result;
+            this.addSubcomponents();
+
+          },
+          (failedResponse)=>{
+
+          }).catch ((e)=>{
+            logger.debug('Something went wrong when calling the service from: '+serviceClass);
+          });
+
+      }
+
+    }
+
+  }),
   Class('ModalController',Controller,{
     dependencies:[],
     component:null,
@@ -39,5 +111,5 @@ Package('org.quickcorp.controllers',[
       component.body.innerHTML = component.body.innerHTML.replace('{{content}}',component.submodal.template);
 
     }
-  })  
+  })
 ]);
